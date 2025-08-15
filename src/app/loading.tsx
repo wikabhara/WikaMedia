e15d834db54f57fb923a2b1d498c71170b3d86e2
wikabhara <wikabhara@gmail.com>
2025-08-16 01:24:10 +0700

    feat: loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+}
